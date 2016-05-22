561af1433e22dd6c7e7a5f66e69656472fb4aba3
Cholerae Hu <choleraehyq@gmail.com>
2016-05-22 18:44:11 +0800

    add json rfc file

--- include/hjson.hpp
+++ include/hjson.hpp
@@ -11,9 +11,9 @@ namespace spirit = boost::spirit;
 template <typename InputIterator>
 struct json_grammar: qi::grammar<InputIterator, json_object(), boost::spirit::ascii::space_type> {
     json_grammar(): json_grammar::base_type(json_object_) {
-        json_object_ = qi::char_('{') 
-                    >> *(json_pair_ % ',')
-                    >> qi::lit('};')
+        json_object_ = qi::lit('{') 
+                    >> -(json_pair_ % ',')
+                    >> qi::lit('}')
                     ;
         json_pair_ = 
         json_string_ %= qi::omit[ qi::char_("\"'")[ spirit::_a = _1 ]]
